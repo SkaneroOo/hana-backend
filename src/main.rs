fbc9f356b99d3a522ee5f5852be3b925e738ef36
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2023-10-30 22:41:26 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use actix_files::NamedFile;
+use actix_files::{Files, NamedFile};
 use actix_web::{get, web::ServiceConfig, Responder};
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
+#[get("/index")]
 async fn index() -> impl Responder {
     NamedFile::open_async("static/index.html").await
 }
 
 #[shuttle_runtime::main]
+#[allow(clippy::unused_async)]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(index);
+        cfg.service(Files::new("/", "static")
+            .show_files_listing()
+            .index_file("index.html")
+            .use_last_modified(true),
+        );
     };
 
     Ok(config.into())
